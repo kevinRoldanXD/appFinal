091f779b7dc1f206bfeeaa81ad1c4224e7c9e861
kevin <kevinsuarez2000@hotmail.com>
2021-07-06 16:53:13 -0400

    make it better

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // connection to db
-mongoose.connect('mongodb://localhost/crud-mongo')
+mongoose.connect('mongodb+srv://kevinRojas:1234@cluster0.x4ln9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
   .then(db => console.log('db connected'))
   .catch(err => console.log(err));
 
